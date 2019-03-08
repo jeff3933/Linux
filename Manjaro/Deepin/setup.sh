145d847118b51f081c25b1e48982d9b498575835
jeff3933 <jeff272@gmail.com>
2019-03-08 04:28:53 -0600

    Update setup.sh

--- Manjaro/Deepin/setup.sh
+++ Manjaro/Deepin/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-soruce ~/bashrc
+soruce ~/.bashrc
 sudo mhwd-kernel -i linux420 # update the kernel to linux 4.20
 sudo pacman -Scc --noconfirm # Run remove cache and remove repositories
 sudo pacman -Syyuu --noconfirm --needed # Run the system updates
